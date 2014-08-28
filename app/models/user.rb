9086724cffbaddf4b3c21c55aec3472a50136f45
Eric Claus Bartholemy <thnukid@users.noreply.github.com>
2014-08-28 15:39:30 +0200

    validation and test of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   has_many :aliases
   has_many :events
   has_many :badges
+
+  validates :username,:email, presence: true
+  validates_uniqueness_of :username, :email
+  validates :password_digest, presence: true, length: {minimum: 8}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'model relations' do
+    it { should have_many(:aliases) }
+    it { should have_many(:events) }
+    it { should have_many(:badges) }
+  end
+
+  describe 'model validation' do
+
+    subject { build(:user)}
+
+    describe 'username validation' do
+      it {should validate_presence_of(:username) }
+      it {should validate_uniqueness_of(:username)}
+    end
+
+    describe 'email validation' do
+      it {should validate_presence_of(:email) }
+      it {should validate_uniqueness_of(:username)}
+    end
+
+    describe 'password validation' do
+      it {should validate_presence_of(:password_digest) }
+      it {should ensure_length_of(:password_digest).is_at_least(8) }
+    end
+
+  end
 end
