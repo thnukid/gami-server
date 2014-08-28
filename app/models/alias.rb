aa0cb154f25bdde6802922d3248ad85c672f4e57
Eric Claus Bartholemy <thnukid@users.noreply.github.com>
2014-08-28 16:00:34 +0200

    validation and test of alias

--- app/models/alias.rb
+++ app/models/alias.rb
@@ -1,3 +1,5 @@
 class Alias < ActiveRecord::Base
   belongs_to :user
+
+  validates :username,:email,:user, presence: true
 end
--- spec/models/alias_spec.rb
+++ spec/models/alias_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Alias, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'model relations' do
+    it { should belong_to(:user) }
+  end
+
+  describe 'model validation' do
+
+    describe "email validation" do
+      it { should validate_presence_of(:username) }
+    end
+
+    describe "username validation" do
+      it { should validate_presence_of(:username) }
+      it { should validate_presence_of(:user) }
+    end
+
+  end
+
+
 end
