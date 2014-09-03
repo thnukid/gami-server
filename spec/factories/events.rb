b71fcc34073244374e5ee5b0231df09a98d4448e
Eric Claus Bartholemy <thnukid@users.noreply.github.com>
2014-09-03 10:02:13 +0200

    trying to write event spec

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,14 +1,14 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  
   factory :event do
     name "MyString"
     data "MyString"
-    user
+    user_id user
   end
 
   factory :invalid_event, parent: :event do
     user nil
   end
+
 end
--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
+require 'pry'
 
 RSpec.describe "Events", :type => :request do
-  describe "GET /events" do
-    it "works! (now write some real specs)" do
-      get events_path
+  describe "GET api/events" do
+    it "return 200" do
+      post api_events_path, :event => attributes_for(:event)
+      binding.pry
       expect(response.status).to be(200)
     end
   end
