789ad74ccb93bfa7bfb3aa3ba744b2ad9a1a888a
Eric Claus Bartholemy <thnukid@users.noreply.github.com>
2014-09-05 15:44:54 +0200

    column changed to text instead of string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140828083415) do
+ActiveRecord::Schema.define(version: 20140905134008) do
 
   create_table "aliases", force: true do |t|
     t.string   "username"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20140828083415) do
 
   create_table "events", force: true do |t|
     t.string   "name"
-    t.string   "data"
+    t.text     "data"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
