0b6473ed8bcb229b95c3f05dd35a90ddbfb0d6ca
Eric Claus Bartholemy <thnukid@users.noreply.github.com>
2014-09-22 10:56:45 +0200

    removed request test, duplicated test

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -1,12 +1,4 @@
 require 'rails_helper'
-require 'pry'
 
 RSpec.describe "Events", :type => :request do
-  describe "GET api/events" do
-    it "return 200" do
-      post api_events_path, :event => attributes_for(:event)
-      binding.pry
-      expect(response.status).to be(200)
-    end
-  end
 end
