d54f8e5fc7bc236a150902a1f32e2cf80ce01492
Eric Claus Bartholemy <thnukid@users.noreply.github.com>
2014-09-22 13:59:38 +0200

    basic gami::rule class

--- lib/gami/gami_rule.rb
+++ lib/gami/gami_rule.rb
@@ -1,5 +1,18 @@
 module Gami
   class Rule
+    attr_reader :description, :badge, :block
 
+    def initialize(description, badge, &block)
+      @description = description
+      @badge = badge
+      @block = block
+    end
+
+    def applies?
+      is_true = true
+      unless self.block.nil?
+        is_true = self.block.call
+      end
+    end
   end
 end
--- spec/gami/gami_rule_spec.rb
+++ spec/gami/gami_rule_spec.rb
@@ -1,5 +1,53 @@
 require 'spec_helper'
 
 describe Gami::Rule do
-  subject{described_class}
+  let(:description) {"You want to play a game?"}
+  let(:badge) {"rookie"}
+  let(:block_false){Proc.new { false }}
+  let(:block_true){Proc.new { true }}
+
+  subject{described_class.new(description, badge, &block_false)}
+
+  describe "#new" do
+    it "initializes correctly with params" do
+     expect(subject).to be_instance_of described_class
+    end
+  end
+
+  context "return attribute" do
+
+  describe "#description" do
+    it "returns the attr" do
+      expect(subject.description).to eq description 
+    end
+  end
+
+  describe "#badge" do
+    it "returns the attr" do
+      expect(subject.badge).to eq badge
+    end
+  end
+
+  describe "#block" do
+    it "returns the attr" do
+      expect(subject.block).to eq block_false
+    end
+  end
+
+  end
+
+  describe "#applies?" do
+    context "block validates to false" do
+      it "validates false" do
+        expect(subject.applies?).to be(false)
+      end
+    end
+
+    context "block validates to true" do
+      before {subject.instance_variable_set(:@block, block_true)}
+      it "validates false" do
+        expect(subject.applies?).to be(true)
+      end
+    end
+  end
 end
