336073521977c4ba1a86938ab3c4082342c109c2
Eric Claus Bartholemy <thnukid@users.noreply.github.com>
2014-09-24 10:55:57 +0200

    add gami files to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 $LOAD_PATH << '../lib'
 require 'factory_girl'
 require 'gami/gami_rule'
+require 'gami/gami_game'
+require 'gami/gami_engine'
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
