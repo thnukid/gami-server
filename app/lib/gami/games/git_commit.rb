ac7d022a0c607dcfa6736ef93dbcc2c8d34e9466
Eric Claus Bartholemy <thnukid@users.noreply.github.com>
2014-09-25 15:42:34 +0200

    add facts to rules, making first rules

--- app/lib/gami/games/git_commit.rb
+++ app/lib/gami/games/git_commit.rb
@@ -5,13 +5,21 @@ class GitCommit
   def self.perform_game(for_user)
     game = GamiGame.new("Gives a user badges based on github push events", 'git:commit', for_user)
 
-    game.rule "Welcome on board!", 'newbie' do
-      true
+    game.rule "Welcome on board, #{for_user.username}!", 'newbie' do
+     fact =  Fact.aggregate_for_user(for_user,"commit_count",1)
+     fact.value <= 5
     end
 
-    game.rule "on board!", 'newbie' do
-      false
+    game.rule "Houston, we have a ...!", 'rookie' do
+     fact = Fact.aggregate_for_user(for_user,"commit_count",1)
+     fact.value <= 10
     end
+
+    game.rule "Look at you, you still see Earth?", 'astronaut' do
+      fact = Fact.aggregate_for_user(for_user,"commit_count",1)
+      fact.value <= 20
+    end
+
     p game.validate?
     p game.apply
   end
