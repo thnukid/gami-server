662915ac110a78f36a427690be0298a9072f215d
Eric Claus Bartholemy <thnukid@users.noreply.github.com>
2014-10-10 14:05:40 +0200

    add header for access-control-allow-origin

--- app/controllers/debug/debug_controller.rb
+++ app/controllers/debug/debug_controller.rb
@@ -1,6 +1,7 @@
 module Debug
   class DebugController < ApplicationController
     skip_before_filter :verify_authenticity_token
+    before_action :set_headers
 
     def events
         @event = Event.all
@@ -19,6 +20,14 @@ module Debug
     def badge
       @badge = Badge.all
       render json: @badge.reverse_order.to_json
+    end
+
+    private
+    def set_headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     end
   end
 end
