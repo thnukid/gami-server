968a39211b3b710b7f81aaeafd41675e28733262
Eric Claus Bartholemy <thnukid@users.noreply.github.com>
2014-10-30 10:42:31 +0100

    load games dir and read as string into array

--- app/lib/gami/gami_engine.rb
+++ app/lib/gami/gami_engine.rb
@@ -1,41 +1,16 @@
 module Gami
   class GamiEngine
+    attr_accessor :games
 
-    def self.run(event)
-      called_game = convert_event_to_class(event.name)
-
-      loaded_games.each do |game|
-        return class_send(called_game, "perform_game", event.user) if called_game.include? @game.to_s
+    def initialize
+      puts "I am a engine"
+      @games = []
+      Dir.glob(File.dirname(__FILE__) + "/games/*.gami") do |gami_file|
+        @games << File.read(gami_file) {
+          |file| file.read
+        }
+        puts @games
       end
     end
-
-    private
-    #available games, TODO: load dynamic
-    def self.loaded_games
-      games = []
-      Dir.glob("games/*.rb") do |game_file|
-        games << game_file.class.name.split('::').last if require_relative game_file
-      end
-      games
-    end
-
-    #calls the defined game
-    def self.class_send(class_name, method, *args)
-      return nil unless Object.const_defined?(class_name)
-      c = Object.const_get(class_name)
-      c.respond_to?(method) ? c.send(method, *args) : nil
-    end
-
-    #expect that the event is seperated by "{service}:{event}"
-    def self.convert_event_to_class(event_name)
-      event_name.titleize.tr(':','').prepend("Gami::")
-    end
-
-    def self.convert_event_to_file(event_name)
-      event_name.downcase.tr(':','_').concat('.rb')
-    end
-
   end
 end
-
-
