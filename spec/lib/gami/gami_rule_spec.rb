431717f2d1836e39d21c8a8fd1a7cb287c32b91f
Eric Claus Bartholemy <thnukid@users.noreply.github.com>
2014-11-20 13:26:28 +0100

    passing rule test spec

--- spec/lib/gami/gami_rule_spec.rb
+++ spec/lib/gami/gami_rule_spec.rb
@@ -1,62 +1,26 @@
 require 'spec_helper'
+require 'rails_helper'
+require 'pry'
 
 describe Gami::Rule do
-  let(:description) {"You want to play a game?"}
-  let(:badge) {"rookie"}
-  let(:block_false){Proc.new { false }}
-  let(:block_true){Proc.new { true }}
-
-  let(:blockX_false){Proc.new { (5*5) == 20 }}
-  let(:blockX_true){Proc.new { (5*5) == 25 }}
-
-  subject{described_class.new(description, badge, &block_false)}
-
-  describe "#new" do
-    it "initializes correctly with params" do
-     expect(subject).to be_instance_of described_class
+  let(:options){{:gteq => 2, :using_property => ['commit']}}
+  let(:bTrue){Gami::Predication.new.build(options)}
+  subject{described_class.new('desc', 'My Badge', 'commit', options ,&bTrue)}
+  describe 'rule' do
+    it 'initializes' do
+      expect(subject).to be_an_instance_of(described_class)
     end
-  end
-
-  context "return attribute" do
-
-  describe "#description" do
-    it "returns the attr" do
-      expect(subject.description).to eq description 
-    end
-  end
-
-  describe "#badge" do
-    it "returns the attr" do
-      expect(subject.badge).to eq badge
-    end
-  end
-
-  describe "#block" do
-    it "returns the attr" do
-      expect(subject.block).to eq block_false
-    end
-  end
-
-  end
-
-  describe "#applies?" do
-    context "block validates to false" do
-      it "validates false" do
-        expect(subject.applies?).to be(false)
+    context 'validates a rule' do
+      let(:event){create :event}
+      it 'it applies false' do
+        event.aggregate_properties(['commit'])
+        expect(subject.applies?(event.user)).to be(false)
       end
-    end
-
-    context "block validates to true" do
-      before {subject.instance_variable_set(:@block, block_true)}
-      it "validates true" do
-        expect(subject.applies?).to be(true)
-      end
-    end
-
-    context "complex block validates to false" do
-      before {subject.instance_variable_set(:@block, blockX_false)}
-      it "validates false" do
-        expect(subject.applies?).to be(false)
+      it 'it applies true' do
+        event.aggregate_properties(['commit'])
+        event.aggregate_properties(['commit'])
+        binding.pry
+        expect(subject.applies?(event.user)).to be(true)
       end
     end
   end
