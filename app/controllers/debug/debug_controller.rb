24ea831e7c072e98ffae6b951191ff45b2ea289e
Eric Claus Bartholemy <thnukid@users.noreply.github.com>
2014-11-20 16:51:03 +0100

    removed duplicated set headers code

--- app/controllers/debug/debug_controller.rb
+++ app/controllers/debug/debug_controller.rb
@@ -1,7 +1,6 @@
 module Debug
   class DebugController < ApplicationController
     skip_before_filter :verify_authenticity_token
-    before_action :set_headers
 
     def events
         @event = Event.all
@@ -25,13 +24,6 @@ module Debug
     def fact
       @fact = Fact.all
       render json: @fact.reverse_order.to_json
-    end
-    private
-    def set_headers
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-      headers['Access-Control-Request-Method'] = '*'
-      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     end
   end
 end
